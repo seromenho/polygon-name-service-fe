87eb1c2da3351bd3851dabbb172410762caf0876
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-04-19 11:47:20 +0100

    mint a domain

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import { ethers } from "ethers";
+import contractAbi from "./utils/contractABI.json";
 
 // Constants
 const TWITTER_HANDLE = "rseromenho";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+// Add the domain you will be minting
+const tld = ".profile";
+const CONTRACT_ADDRESS = "0x589B8EEc0D432f692B12d93dBF544d74DC1460a4";
 
 const App = () => {
   //Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
   const [currentAccount, setCurrentAccount] = useState("");
+  const [domain, setDomain] = useState("");
+  const [record, setRecord] = useState("");
 
   // Implement your connectWallet method here
   const connectWallet = async () => {
@@ -28,6 +35,64 @@ const App = () => {
       // Boom! This should print out public address once we authorize Metamask.
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const mintDomain = async () => {
+    // Don't run if the domain is empty
+    if (!domain) {
+      return;
+    }
+    // Alert the user if the domain is too short
+    if (domain.length < 3) {
+      alert("Domain must be at least 3 characters long");
+      return;
+    }
+    // Calculate price based on length of domain (change this to match your contract)
+    // 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+    const price =
+      domain.length === 3 ? "0.5" : domain.length === 4 ? "0.3" : "0.1";
+    console.log("Minting domain", domain, "with price", price);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait();
+
+        // Check if the transaction was successfully completed
+        if (receipt.status === 1) {
+          console.log(
+            "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          // Set the record for the domain
+          tx = await contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log(
+            "Record set! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          setRecord("");
+          setDomain("");
+        } else {
+          alert("Transaction failed! Please try again");
+        }
+      }
     } catch (error) {
       console.log(error);
     }
@@ -74,6 +139,43 @@ const App = () => {
     </div>
   );
 
+  // Form to enter domain name and data
+  const renderInputForm = () => {
+    return (
+      <div className="form-container">
+        <div className="first-row">
+          <input
+            type="text"
+            value={domain}
+            placeholder="domain"
+            onChange={(e) => setDomain(e.target.value)}
+          />
+          <p className="tld"> {tld} </p>
+        </div>
+
+        <input
+          type="text"
+          value={record}
+          placeholder="whats ur ninja power"
+          onChange={(e) => setRecord(e.target.value)}
+        />
+
+        <div className="button-container">
+          <button className="cta-button mint-button" onClick={mintDomain}>
+            Mint
+          </button>
+          <button
+            className="cta-button mint-button"
+            disabled={null}
+            onClick={null}
+          >
+            Set data
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   // This runs our function when the page loads.
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -95,6 +197,8 @@ const App = () => {
 
         {/* Hide the connect button if currentAccount isn't empty*/}
         {!currentAccount && renderNotConnectedContainer()}
+        {/* Render the input form if an account is connected */}
+        {currentAccount && renderInputForm()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
